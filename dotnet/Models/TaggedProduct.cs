2ded159945b62c097604d90dde031ac859eb4372
Jorge Espinosa Aldana <jorgeespinosa@Jorges-MacBook-Air.local>
2021-10-31 03:51:52 -0300

    test123

--- dotnet/Models/TaggedProduct.cs
+++ dotnet/Models/TaggedProduct.cs
@@ -7,13 +7,7 @@ namespace DotNetService.Models
     {
         public int Id { get; }
         public string Name { get;}
-        
-        // This is an oversimplified way to automatically create search tags for the sake of providing an interesting example.
-        public string[] Tags => Name?.Split(" ", StringSplitOptions.RemoveEmptyEntries)
-            .Where(s => s.Length > 3)
-            .Select(s=> s.ToLowerInvariant())
-            .ToArray() ?? new string[0];
 
-        public TaggedProduct(Product product) => (Id, Name) = (product.Id, product.Name);
+        public TaggedProduct(Product product) => (Id, Name) = (1, "hola");
     }
 }

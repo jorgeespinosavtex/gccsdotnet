a3626bb0e2f764a411fe71b493f33fcc422d55fc
Jorge Espinosa Aldana <jorgeespinosa@Jorges-MacBook-Air.local>
2021-10-31 03:46:59 -0300

    test

--- dotnet/Clients/VtexHttpClient.cs
+++ dotnet/Clients/VtexHttpClient.cs
@@ -5,7 +5,6 @@ using Vtex.Api.Context;
 
 namespace DotNetService.Clients
 {
-    // A helper class to configure a HttpClient according to VTEX Core Commerce api specs.
     public static class VtexHttpClient
     {
         private const string AuthorizationHeader = "VtexIdclientAutCookie";
@@ -17,17 +16,18 @@ namespace DotNetService.Clients
             var serviceContext = serviceProvider.GetService<IIOServiceContext>();
             
             // So we use it to configure the base url for our core commerce api client
-            var env = serviceContext.Vtex.JanusEnv == "beta" ? "beta" : "stable";
-            var baseUri = $"https://{serviceContext.Vtex.Account}.vtexcommerce{env}.com.br";
+            var baseUri = $"https://vtexid.vtexcommercestable.com.br";
                 
             if (!Uri.TryCreate($"{baseUri}", UriKind.Absolute, out var uri))
                 throw new Exception($"Invalid Vtex baseUri: {baseUri}");
             
             client.BaseAddress = uri;
-            
+
             // Here we ensure we are relaying the correct header parameters for the app to work
             client.DefaultRequestHeaders.Add(AuthorizationHeader, serviceContext.Vtex.AuthToken);
             client.DefaultRequestHeaders.Add(UseHttpsHeader, "true");
+            client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            client.DefaultRequestHeaders.Add("Cookie", "VtexIdclientAutCookie=eyJhbGciOiJFUzI1NiIsImtpZCI6IjhCMjY5MjAwRUZDNkZEM0UwRjRFRDU2MTE2RTlFOTYwQTAzQ0Y5OEEiLCJ0eXAiOiJqd3QifQ.eyJzdWIiOiJ2dGV4YXBwa2V5LWRlY29yZXN0LVZJV0FEViIsImFjY291bnQiOiJ2dGV4IiwiYXVkaWVuY2UiOiJhZG1pbiIsImV4cCI6MTYzNTY4MDkzMywidXNlcklkIjoiNDI0MmY3ZmUtNjFkOC00MzY2LThiZDAtZGI1YWNmMzk5YTZmIiwiaWF0IjoxNjM1NjU5MzMzLCJpc3MiOiJ0b2tlbi1lbWl0dGVyIiwianRpIjoiOWIxNTE0YzYtYjRlNi00NDdhLWE1NDAtMjAyMzgxZTY4OGYzIn0.7UMphYVT0ocaR2nO_Q7NXZ2W4fLWKtgZQtk2ylfoeWENeyvydmZ3KJGVx_f2Pzr-rEiHeidy-JIfXj5QXVSxEQ;");
         }
     }
 }
--- dotnet/Data/IProductRepository.cs
+++ dotnet/Data/IProductRepository.cs
@@ -7,8 +7,8 @@ namespace DotNetService.Data
     public interface IProductRepository
     {
         // We are using refit so the request method, relative url, custom headers and parameters are set through these attributes
-        [Headers("Cache-Control: no-cache")]
-        [Get("/api/catalog/pvt/product/{productId}")]
+        [Headers("Cookie: VtexIdclientAutCookie=eyJhbGciOiJFUzI1NiIsImtpZCI6IjhCMjY5MjAwRUZDNkZEM0UwRjRFRDU2MTE2RTlFOTYwQTAzQ0Y5OEEiLCJ0eXAiOiJqd3QifQ.eyJzdWIiOiJ2dGV4YXBwa2V5LWRlY29yZXN0LVZJV0FEViIsImFjY291bnQiOiJ2dGV4IiwiYXVkaWVuY2UiOiJhZG1pbiIsImV4cCI6MTYzNTY4MDkzMywidXNlcklkIjoiNDI0MmY3ZmUtNjFkOC00MzY2LThiZDAtZGI1YWNmMzk5YTZmIiwiaWF0IjoxNjM1NjU5MzMzLCJpc3MiOiJ0b2tlbi1lbWl0dGVyIiwianRpIjoiOWIxNTE0YzYtYjRlNi00NDdhLWE1NDAtMjAyMzgxZTY4OGYzIn0.7UMphYVT0ocaR2nO_Q7NXZ2W4fLWKtgZQtk2ylfoeWENeyvydmZ3KJGVx_f2Pzr-rEiHeidy-JIfXj5QXVSxEQ")]
+        [Post("/Export/GenerateExport?selectedFields=clientName,clientRut,companyRut,creationDate,giftcardAmount,giftcardDeliveryType,giftcardQuantity,giftcardRestriction,giftcardTaxes,orderNumber&idDataEntity=GD&fullText=&searchQuery=orderNumber%3D100000000&recipients=jorge.espinosa@vtex.com.br")]
         Task<Product> GetProduct(int productId);
     }
 }
